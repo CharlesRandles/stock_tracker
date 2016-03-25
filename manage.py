192d061cba28ac56d33c98552bb6458eb1851749
Charles Randles <charles.randles@gmail.com>
2016-03-25 14:08:53 +1000

    manage utility now shows and buys

---
+++ manage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""Script to record buying, selling and unselling of shares"""
+
+import sys
+import
+
+if __name__=="__main__"":
+    main()
---
+++ stockutils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+"""Utility functions"""
+import datetime
+
+DB_FILE="db/holdings.db"
+timeFormat = fmt="%Y-%m-%d %H:%M:%S"
+
+#Helper for messing with timezones
+def parseOffset(offset):
+    hours = int(offset[0:3])
+    minutes = int(offset[3:])
+    return datetime.timedelta(hours=hours, minutes=minutes)
+
+def toUTC(time, offset):
+    return time - parseOffset(offset)
+
+def nowUTC():
+    offset = configdb.getConfig('server_utcoffset')
+    now_utc=toUTC(datetime.datetime.now(), offset)
+    return now_utc.strftime(timeFormat)
