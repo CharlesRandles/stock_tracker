e27d6c2496533e19596b08f923019e2f42189c76
CharlesRandles <charles.randles@gmail.com>
2016-07-19 11:27:57 +1000

    Corrected typo

--- summary.py
+++ summary.py
@@ -29,7 +29,7 @@ def groupSummary():
        where sale_date is null
        group by symbol
        order by symbol;"""
-    s="<p><table><thead><th>Symbol</th><th>Purchase Cost</th><th>Value</th><th>% Gain</th></thead>"
+    s="<p><table><thead><th>Symbol</th><th>Purchase Cost</th><th>Value</th><th>Gain</th></thead>"
     cursor = stockdb.getCursor()
     cursor.execute(sql,())
     for r in cursor:
@@ -57,7 +57,7 @@ def body():
         <a href="stocks.py">Full Report</a>
     </body>
 </html>
-    """.format(groupSummary()),
+    """.format(groupSummary(),
                       refresh_form())
     return b
 
